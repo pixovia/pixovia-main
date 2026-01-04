f5d9bf62822fa89beb8dab808d01ceb3086823a7
pixovia <pixoviallc@gmail.com>
2026-01-04 22:27:46 +0530

    Add files via upload

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -60,7 +60,7 @@ export async function onRequest(context) {
     apps.forEach(app => urls.push({ loc: `${baseUrl}/store/app/${app.id}`, priority: '0.7', changefreq: 'weekly' }));
     files.forEach(file => urls.push({ loc: `${baseUrl}/library/file/${file.id}`, priority: '0.6', changefreq: 'monthly' }));
     albums.forEach(album => urls.push({ loc: `${baseUrl}/library/album/${album.id}`, priority: '0.6', changefreq: 'monthly' }));
-    movies.forEach(movie => urls.push({ loc: `${baseUrl}/movies/${movie.id}`, priority: '0.7', changefreq: 'weekly' }));
+    movies.forEach(movie => urls.push({ loc: `${baseUrl}/movies/${movie.id}`, priority: '0.8', changefreq: 'weekly' }));
     music.forEach(track => urls.push({ loc: `${baseUrl}/music/player/${track.id}`, priority: '0.6', changefreq: 'monthly' }));
     sports.forEach(sport => urls.push({ loc: `${baseUrl}/sports/${sport.id}`, priority: '0.7', changefreq: 'hourly' }));
     courses.forEach(course => urls.push({ loc: `${baseUrl}/learn/${course.id}`, priority: '0.6', changefreq: 'monthly' }));
