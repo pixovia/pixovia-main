2ea8fc357252baded53a6eb6c40862e6fc968637
pixovia <pixoviallc@gmail.com>
2026-01-04 22:14:46 +0530

    Add files via upload

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -22,7 +22,7 @@ export async function onRequest(context) {
     { loc: `${baseUrl}/library`, priority: '0.9', changefreq: 'daily' },
     { loc: `${baseUrl}/movies`, priority: '0.9', changefreq: 'daily' },
     { loc: `${baseUrl}/music`, priority: '0.9', changefreq: 'daily' },
-    { loc: `${baseUrl}/sports`, priority: '0.8', changefreq: 'daily' },
+    { loc: `${baseUrl}/sports`, priority: '0.9', changefreq: 'hourly' },
     { loc: `${baseUrl}/learn`, priority: '0.8', changefreq: 'weekly' },
     { loc: `${baseUrl}/downloader`, priority: '0.7', changefreq: 'weekly' },
     { loc: `${baseUrl}/contact`, priority: '0.5', changefreq: 'monthly' },
@@ -43,16 +43,26 @@ export async function onRequest(context) {
       librarySupabase.from('albums').select('id').then(r => r.data || []),
       librarySupabase.from('movie_audio_learn').select('id').eq('type', 'movie').then(r => r.data || []),
       librarySupabase.from('movie_audio_learn').select('id').eq('type', 'audio').then(r => r.data || []),
-      librarySupabase.from('sports').select('id').then(r => r.data || []),
+      librarySupabase.from('sports').select('id, type').then(r => r.data || []),
       librarySupabase.from('movie_audio_learn').select('id').eq('type', 'learn').then(r => r.data || [])
     ]);
+
+    // Add sports type pages
+    const sportsTypes = [...new Set(sports.map(s => s.type).filter(Boolean))];
+    sportsTypes.forEach(type => {
+      urls.push({ 
+        loc: `${baseUrl}/sports/type/${encodeURIComponent(type)}`, 
+        priority: '0.8', 
+        changefreq: 'hourly' 
+      });
+    });
 
     apps.forEach(app => urls.push({ loc: `${baseUrl}/store/app/${app.id}`, priority: '0.7', changefreq: 'weekly' }));
     files.forEach(file => urls.push({ loc: `${baseUrl}/library/file/${file.id}`, priority: '0.6', changefreq: 'monthly' }));
     albums.forEach(album => urls.push({ loc: `${baseUrl}/library/album/${album.id}`, priority: '0.6', changefreq: 'monthly' }));
     movies.forEach(movie => urls.push({ loc: `${baseUrl}/movies/${movie.id}`, priority: '0.7', changefreq: 'weekly' }));
     music.forEach(track => urls.push({ loc: `${baseUrl}/music/player/${track.id}`, priority: '0.6', changefreq: 'monthly' }));
-    sports.forEach(sport => urls.push({ loc: `${baseUrl}/sports/${sport.id}`, priority: '0.6', changefreq: 'weekly' }));
+    sports.forEach(sport => urls.push({ loc: `${baseUrl}/sports/${sport.id}`, priority: '0.7', changefreq: 'hourly' }));
     courses.forEach(course => urls.push({ loc: `${baseUrl}/learn/${course.id}`, priority: '0.6', changefreq: 'monthly' }));
   } catch (error) {
     console.error('Error fetching data:', error);
