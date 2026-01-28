21442b5688db7d36ad626518c5ea9d8ad4156ea1
pixovia <pixoviallc@gmail.com>
2026-01-28 21:27:27 +0530

    Update Profile.js

--- src/library/pages/Profile.js
+++ src/library/pages/Profile.js
@@ -153,7 +153,7 @@ const Profile = () => {
     } else if (fileType.includes('exe')) {
       iconUrl = 'https://cdn-icons-png.flaticon.com/512/29/29614.png';
     } else {
-      iconUrl = 'https://via.placeholder.com/200x200/333/fff?text=FILE';
+      iconUrl = 'https://cdn-icons-png.flaticon.com/512/702/702820.png';
     }
     
     return (
