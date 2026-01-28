c625ab12202ccd2fa6bae2c8d0c8b1eac0370fdb
pixovia <pixoviallc@gmail.com>
2026-01-28 21:26:10 +0530

    Update Files.js

--- src/library/pages/Files.js
+++ src/library/pages/Files.js
@@ -176,7 +176,7 @@ const Files = () => {
     } else if (fileType.includes('exe')) {
       iconUrl = 'https://cdn-icons-png.flaticon.com/512/29/29614.png';
     } else {
-      iconUrl = 'https://via.placeholder.com/200x200/333/fff?text=FILE';
+      iconUrl = 'https://cdn-icons-png.flaticon.com/512/702/702820.png';
     }
     
     return (
