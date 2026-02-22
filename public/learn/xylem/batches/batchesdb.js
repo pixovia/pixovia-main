75e26b841d9bfc1daf10984171389edbe80aa451
pixovia <pixoviallc@gmail.com>
2026-02-22 14:43:25 +0530

    Add files via upload

---
+++ public/learn/xylem/batches/batchesdb.js
@@ -0,0 +1,42 @@
+// batchesdb.js
+export const banners = [
+    {
+        image: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/f5a5ce2d-3645-4874-9816-add955a89de3.jpg",
+        link: "https://xylem.live/offers/edufest"
+    },
+    {
+        image: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/530fbe1c-1ea5-4e38-a228-e7beef2ea480.jpeg",
+        link: "https://xylem.live/parent-app"
+    }
+];
+
+export const batchesData = [
+    {
+        id: "batch_001",
+        exam: "KEAM",
+        class: "12th",
+        type: "Online",
+        title: "KEAM - ENDGAME [2026]",
+        tagline: "Targeted Crash Batch For KEAM 2026 Aspirants",
+        logo: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/70ef6932-68cc-41f4-9114-5b1f96968cff.jpg",
+        start: "10 Feb 2026",
+        end: "30 Apr 2026",
+        features: "Live Lectures | Tests & many more",
+        orgPrice: 6713,
+        nowPrice: 0
+    },
+    {
+        id: "batch_002",
+        exam: "JEE",
+        class: "Repeaters",
+        type: "Online",
+        title: "LMRP - JEE PHASE 2 [2026]",
+        tagline: "Targeted batch for JEE Main Phase 2 2026 Aspirants",
+        logo: "https://static.pw.live/5eb393ee95fab7468a79d189/ADMIN/e7f0780f-e538-4195-adb0-897c8026d85a.jpeg",
+        start: "15 Feb 2026",
+        end: "30 Jun 2026",
+        features: "Live Lectures | Recorded Lectures | DPPs",
+        orgPrice: 5058,
+        nowPrice: 0
+    }
+];
