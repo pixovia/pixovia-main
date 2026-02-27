249d0da6e539ce60d6211f7cf88528b0f81786a4
pixovia <pixoviallc@gmail.com>
2026-02-27 21:16:25 +0530

    Update Files.js

--- src/library/pages/Files.js
+++ src/library/pages/Files.js
@@ -335,7 +335,7 @@ const Files = () => {
           <p>No files available{currentType !== 'all' ? ` for ${currentType} type` : ''}.</p>
         </div>
       )}
-     </div> {/* closes main wrapper */}
+    </div> {/* closes main wrapper */}
   );
 };
 
